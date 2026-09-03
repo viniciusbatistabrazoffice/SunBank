415d37b6886eec8653da0ba013f5b8d99775fa72
viniciusbatistadev <viniciusbatistabrazoffice@gmail.com>
2026-09-03 11:17:40 -0300

    Implementando a injecao de dependencia

--- backend/src/main/java/com/backend/service/OperacaoServiceImpl.java
+++ backend/src/main/java/com/backend/service/OperacaoServiceImpl.java
@@ -1,11 +1,17 @@
 package com.backend.service;
 
 import com.backend.entity.Operacao;
+import com.backend.repository.OperacaoRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class OperacaoServiceImpl implements OperacaoService{
+    private final OperacaoRepository operacaoRepository;
+
+    public OperacaoServiceImpl(OperacaoRepository operacaoRepository) {
+        this.operacaoRepository = operacaoRepository;
+    }
 
     @Override
     public Operacao save(Operacao operacao) {
