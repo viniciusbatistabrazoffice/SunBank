aef140dec29f913b652f7845685964a2bb4aedcf
viniciusbatistadev <viniciusbatistabrazoffice@gmail.com>
2026-09-03 11:09:50 -0300

    Implementando a interface na operacao service impl

---
+++ backend/src/main/java/com/backend/service/OperacaoServiceImpl.java
@@ -0,0 +1,29 @@
+package com.backend.service;
+
+import com.backend.entity.Operacao;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OperacaoServiceImpl implements OperacaoService{
+
+    @Override
+    public Operacao save(Operacao operacao) {
+        return null;
+    }
+
+    @Override
+    public Optional<List<Operacao>> list(Operacao operacao) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Operacao update(Operacao operacao) {
+        return null;
+    }
+
+    @Override
+    public Operacao delete(Operacao operacao) {
+        return null;
+    }
+}
